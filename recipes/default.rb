e0fc4e6d298206cab6a966d5bdf3bd0911b845c8
Myles Carrick <myles@mylescarrick.com>
2012-10-25 21:08:20 +1100

    edging closer

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+app = data_bag_item('webapps', 'aaibs')
+
+file "/home/#{app['deploy_user']}/.ssh/id_rsa" do
+  owner app['deploy_user']
+  mode "0600"
+  action :create
+  content app['deploy_private_key']
+end
+
+application "aaibs" do
+  path "/var/webapps/aaibs"
+  owner "deploy"
+  group "deploy"
+
+  repository "git@github.com:mylescarrick/aaibs3.git"
+  revision "master"
+
+  rails do
+    # Rails-specific configuration
+  end
+
+  passenger_apache2 do
+    server_aliases ["beta.aaibs.org"]
+  end
+end
